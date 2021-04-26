6958927662d868c4da8158d88c558a6ab2704daf
khokonarifulislam64 <khokonarifulislam64@gmail.com>
2021-04-26 14:53:37 +0600

    first commit

---
+++ src/main/java/com/ultraangle/springboot/api/exaGroupyBackend/ExaGroupyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ultraangle.springboot.api.exaGroupyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaGroupyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaGroupyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultraangle/springboot/api/exaGroupyBackend/ExaGroupyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultraangle.springboot.api.exaGroupyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaGroupyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
